0d21c5922cf9cfb7cf4c495ce34515eb922e5a16
Max <maksim.gil.92@gmail.ru>
2020-03-23 08:50:23 +0300

    finish

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-let = 8;
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату?", "YYYY-MM-DD");
+let expense = prompt("");
+let coast = prompt("");
+let expense2 = prompt("");
+let coast2 = prompt("");
+
+let expenses = {
+    expense: coast,
+    expense2: coast2
+};
+
+let appData = {
+    budget: money,
+    timeData: money,
+    expenses,
+    optionalExpenses: "",
+    income: "",
+    savings: false
+};
+
+alert("money/30");
